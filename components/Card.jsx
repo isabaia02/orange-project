adfcd8825e47f64b15a7d40eb1f2b79d0e793197
isabaia02 <isacbaia.2005@gmail.com>
2025-05-31 18:26:14 -0300

    loginForm and css updates

--- components/Card.jsx
+++
@@ -1,8 +0,0 @@
-export default function Card({ title, imageUrl }) {
-    return (
-      <div className="card"> 
-        <img src={imageUrl} alt={title} />
-        <h3>{title}</h3>
-      </div>
-    );
-}
